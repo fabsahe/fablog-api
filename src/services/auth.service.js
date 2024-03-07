dda7983d9a178cfb3a282292d57bcbe32b5c5ced
Fabián SH <fabian@adminprime.com>
2024-03-06 18:41:36 -0600

    manejo de error en login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,13 @@
 import { pool } from '../db.js'
 
 const login = async (email) => {
-  const query = 'SELECT u.*, r.name as role FROM users u JOIN roles r ON u.role_id = r.id WHERE u.email = ?'
-  const [user] = await pool.query(query, [email])
-  return user && user.length > 0 ? user[0] : null
+  try {
+    const query = 'SELECT u.*, r.name as role FROM users u JOIN roles r ON u.role_id = r.id WHERE u.email = ?'
+    const [user] = await pool.query(query, [email])
+    return user && user.length > 0 ? user[0] : null
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default { login }
